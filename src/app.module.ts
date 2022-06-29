b03c846589118c9a3e4a0f32c89691c3d50e4314
Taiwo <ayeola05@gmail.com>
2022-06-29 17:16:32 +0100

    Appended my mongodb_uri to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
+import "dotenv/config"
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UsersModule, 
-    MongooseModule.forRoot("mongodb://localhost/new-nestjs-crud"), 
-    AuthModule,
-    ConfigModule.forRoot()
+    MongooseModule.forRoot(process.env.MONGODB_URI), 
+    AuthModule
   ],
   controllers: [],
   providers: [],
